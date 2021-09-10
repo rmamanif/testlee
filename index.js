a4d40b3d21ae70d4d8cc7852a4e8a2ceaaf08b77
Richard Mamani <richard.mamani@tecsup.edu.pe>
2021-09-10 12:59:08 -0500

    asdasd

--- index.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-fs = require('fs')
-
-http.createServer(function(solicitud,respuesta){
-    fs.readFile('./index1.html',function(error,html){
-        respuesta.write(html)
-        respuesta.end()
-    })
-}).listen(8080)
